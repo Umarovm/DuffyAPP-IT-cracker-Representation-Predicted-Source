610aa2652cf67265e2aefd9b18a5f2788cf225fc
Umarovm <giyasumarov@icloud.com>
2020-12-30 17:55:01 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
+#include <time.h>
 #include <stdlib.h>
 
 #define PIN 274252
@@ -12,11 +7,10 @@ int main() {
 	puts("6 Digit Passcode? Let's crack it...\nWait 3 seconds — DuffyAPP‐IT");
 	sleep(3);
 	for (int i = 0; i <= 999999; i++) {
-		printf("TRYING PIN -> %d\n",i);
+		printf("TRYING PIN -> %d\n", i);
 		if (i == PIN) {
-			printf("FOUND — PIN IS -> %d\n",i);
+			printf("FOUND — PIN IS -> %d\n", i);
 			exit(0);
 		}
 	}
-	return 1;
 }
